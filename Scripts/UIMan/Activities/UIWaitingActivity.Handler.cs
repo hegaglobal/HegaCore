314c64790a3a2011bb5e93cbba58317fb8620c26
Laicasaane <laicasaane@outlook.com>
2020-08-15 14:11:09 +0700

    Update

---
+++ Scripts/UIMan/Activities/UIWaitingActivity.Handler.cs
@@ -0,0 +1,8 @@
+using UnuGames;
+
+namespace Game.UI
+{
+    public partial class UIWaitingActivity : UIActivity
+    {
+    }
+}
---
+++ Scripts/UIMan/Activities/UIWaitingActivity.cs
@@ -0,0 +1,13 @@
+
+using UnuGames;
+using UnuGames.MVVM;
+
+// This code is generated automatically by UIMan - UI Generator, please do not modify!
+
+namespace Game.UI
+{
+    public sealed partial class UIWaitingActivity
+    {
+
+    }
+}
