26e54f4922ac082507d0b9404bf15d4aed653770
Laicasaane <laicasaane@outlook.com>
2020-09-18 15:38:45 +0700

    Update

---
+++ Scripts/Common/GID.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HegaCore
+{
+    public readonly struct GID : IEquatableReadOnlyStruct<GID>, IComparableReadOnlyStruct<GID>
+    {
+        public readonly uint Id;
+
+        private GID(uint id)
+            => this.Id = id;
+
+        public override bool Equals(object obj)
+            => obj is GID other &&
+               this.Id == other.Id;
+
+        public bool Equals(in GID other)
+            => this.Id == other.Id;
+
+        public bool Equals(GID other)
+            => this.Id == other.Id;
+
+        public override int GetHashCode()
+            => 2108858624 + this.Id.GetHashCode();
+
+        public int CompareTo(in GID other)
+            => this.Id.CompareTo(other.Id);
+
+        public int CompareTo(GID other)
+            => this.Id.CompareTo(other.Id);
+
+        private static uint _GID = 0;
+
+        public static GID Get()
+            => new GID(_GID++);
+
+        public static void Reset()
+            => _GID = 0;
+
+        public static implicit operator uint(in GID value)
+            => value.Id;
+
+        public static bool operator ==(in GID lhs, in GID rhs)
+            => lhs.Id == rhs.Id;
+
+        public static bool operator !=(in GID lhs, in GID rhs)
+            => lhs.Id != rhs.Id;
+
+        public static bool operator >(in GID lhs, in GID rhs)
+            => lhs.Id > rhs.Id;
+
+        public static bool operator <(in GID lhs, in GID rhs)
+            => lhs.Id < rhs.Id;
+    }
+}
