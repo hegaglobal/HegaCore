47cb78ccb3603620041232d53643b354c37464c7
Laicasaane <laicasaane@outlook.com>
2020-10-01 17:21:15 +0700

    Update

--- Scripts/Common/CellGrid/ReadCellGrid.cs
+++ Scripts/Common/CellGrid/ReadCellGrid.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Grid;
 using System;
+using System.Diagnostics;
 
 namespace HegaCore
 {
@@ -77,7 +78,7 @@ namespace HegaCore
 
         private void Filter(in GridIndex? pivot, List<Cell> cells, ICollection<Cell> output, in CellModes modes)
         {
-            if (pivot.HasValue)
+            if (cells.Count > 0 && pivot.HasValue)
             {
                 switch (modes.Diagonal)
                 {
@@ -91,26 +92,26 @@ namespace HegaCore
                 }
             }
 
-            if (modes.Occupied == CellMode.Include)
-                return;
-
-            var occupied = HashSetPool<GridIndex>.Get();
-            this.occupier.GetOccupied(occupied);
-
-            switch (modes.Occupied)
+            if (cells.Count > 0 && modes.Occupied != CellMode.Include)
             {
-                case CellMode.Exclude:
-                    RemoveOccupied(cells, occupied);
-                    break;
+                var occupied = HashSetPool<GridIndex>.Get();
+                this.occupier.GetOccupied(occupied);
 
-                case CellMode.Only:
-                    OnlyOccupied(cells, occupied);
-                    break;
+                switch (modes.Occupied)
+                {
+                    case CellMode.Exclude:
+                        RemoveOccupied(cells, occupied);
+                        break;
+
+                    case CellMode.Only:
+                        OnlyOccupied(cells, occupied);
+                        break;
+                }
+
+                HashSetPool<GridIndex>.Return(occupied);
             }
 
             output.AddRange(cells, false, true);
-
-            HashSetPool<GridIndex>.Return(occupied);
             ListPool<Cell>.Return(cells);
         }
 
@@ -168,14 +169,6 @@ namespace HegaCore
             Filter(pivot, cells, output, modes ?? CellModes.Include);
         }
 
-        public void GetCells(in GridIndex pivot, in GridRange range, ICollection<Cell> output, in CellModes? modes = null)
-        {
-            var cells = ListPool<Cell>.Get();
-            this.data.GetValues(range, cells);
-
-            Filter(pivot, cells, output, modes ?? CellModes.Include);
-        }
-
         public void GetCells(in GridIndex pivot, in GridIndexRange range, ICollection<Cell> output, in CellModes? modes = null)
         {
             var cells = ListPool<Cell>.Get();
@@ -208,14 +201,6 @@ namespace HegaCore
             Filter(null, cells, output, modes ?? CellModes.Include);
         }
 
-        public void GetCells(in GridRange range, ICollection<Cell> output, in CellModes? modes = null)
-        {
-            var cells = ListPool<Cell>.Get();
-            this.data.GetValues(range, cells);
-
-            Filter(null, cells, output, modes ?? CellModes.Include);
-        }
-
         public void GetCells(in GridIndexRange range, ICollection<Cell> output, in CellModes? modes = null)
         {
             var cells = ListPool<Cell>.Get();
@@ -240,11 +225,45 @@ namespace HegaCore
             Filter(null, cells, output, modes ?? CellModes.Include);
         }
 
+        public void GetRanges(in GridIndexRange pivot, int size, int step, ICollection<GridIndexRange> output,
+                              in CellModes? modes = null)
+        {
+            var slice = this.clampedSize.IndexRange(pivot, size);
+            var ranges = ListPool<GridIndexRange>.Get();
+            this.clampedSize.IndexRanges(slice, size, step, ranges);
+
+            Filter(pivot, ranges, modes ?? CellModes.Include);
+            output.AddRange(ranges);
+
+            ListPool<GridIndexRange>.Return(ranges);
+        }
+
         public bool TryGetRandomCell(out Cell output, in CellModes? modes = null)
         {
             var cells = ListPool<Cell>.Get();
             GetCells(cells, modes);
 
+            return TryGetRandomCell(cells, out output);
+        }
+
+        public bool TryGetRandomCell(in GridIndexRange range, out Cell output, in CellModes? modes = null)
+        {
+            var cells = ListPool<Cell>.Get();
+            GetCells(range, cells, modes);
+
+            return TryGetRandomCell(cells, out output);
+        }
+
+        public bool TryGetRandomCell(in GridIndex pivot, in GridIndexRange range, out Cell output, in CellModes? modes = null)
+        {
+            var cells = ListPool<Cell>.Get();
+            GetCells(pivot, range, cells, modes);
+
+            return TryGetRandomCell(cells, out output);
+        }
+
+        private bool TryGetRandomCell(List<Cell> cells, out Cell output)
+        {
             if (cells.Count <= 0)
             {
                 ListPool<Cell>.Return(cells);
@@ -269,7 +288,21 @@ namespace HegaCore
 
             var cells = ListPool<Cell>.Get();
             GetCells(cells, modes);
+            GetRandomCells(count, output, cells);
+        }
 
+        public void GetRandomCells(in GridIndexRange range, int count, ICollection<Cell> output, in CellModes? modes = null)
+        {
+            if (count <= 0)
+                return;
+
+            var cells = ListPool<Cell>.Get();
+            GetCells(range, cells, modes);
+            GetRandomCells(count, output, cells);
+        }
+
+        private static void GetRandomCells(int count, ICollection<Cell> output, List<Cell> cells)
+        {
             if (cells.Count > 0)
             {
                 var random = ListPool<Cell>.Get();
@@ -315,16 +348,94 @@ namespace HegaCore
 
             var ranges = ListPool<GridIndexRange>.Get();
             this.clampedSize.IndexRanges(size, step, ranges);
+            Filter(null, ranges, modes ?? CellModes.Include);
 
-            var cModes = modes ?? CellModes.Include;
+            return TryGetRandomRange(ref range, ranges);
+        }
 
-            if (ranges.Count > 0 && cModes.Occupied != CellMode.Include)
+        public bool TryGetRandomRange(in GridIndex pivot, int extend, int size, int step,
+                                      out GridIndexRange range, in CellModes? modes = null)
+            => TryGetRandomRange(pivot, this.clampedSize.IndexRange(pivot, extend), size, step,
+                                 out range, modes);
+
+        public bool TryGetRandomRange(in GridIndex pivot, int lowerExtend, int upperExtend, int size, int step,
+                                      out GridIndexRange range, in CellModes? modes = null)
+            => TryGetRandomRange(pivot, this.clampedSize.IndexRange(pivot, lowerExtend, upperExtend), size, step,
+                                 out range, modes);
+
+        public bool TryGetRandomRange(in GridIndex pivot, in GridIndex lowerExtend, in GridIndex upperExtend, int size, int step,
+                                      out GridIndexRange range, in CellModes? modes = null)
+            => TryGetRandomRange(pivot, this.clampedSize.IndexRange(pivot, lowerExtend, upperExtend), size, step,
+                                 out range, modes);
+
+        public bool TryGetRandomRange(in GridIndex pivot, in GridIndexRange pivotRange, int size, int step,
+                                      out GridIndexRange range, in CellModes? modes = null)
+        {
+            range = default;
+
+            if (size < 1)
+                return false;
+
+            if (size == 1)
+            {
+                var slice = this.clampedSize.IndexRange(pivotRange, size);
+
+                if (TryGetRandomCell(pivot, slice, out var cell, modes))
+                {
+                    range = new GridIndexRange(cell, cell);
+                    return true;
+                }
+
+                return false;
+            }
+
+            var ranges = ListPool<GridIndexRange>.Get();
+            GetRanges(pivotRange, size, step, ranges, modes);
+
+            return TryGetRandomRange(ref range, ranges);
+        }
+
+        private static bool TryGetRandomRange(ref GridIndexRange range, List<GridIndexRange> ranges)
+        {
+            var result = false;
+
+            if (ranges.Count > 0)
+            {
+                var random = ListPool<GridIndexRange>.Get();
+                random.AddRange(ranges.Randomize());
+                range = random[UnityRandom.Range(0, random.Count)];
+
+                ListPool<GridIndexRange>.Return(random);
+                result = true;
+            }
+
+            ListPool<GridIndexRange>.Return(ranges);
+            return result;
+        }
+
+        private void Filter(in GridIndexRange? pivot, List<GridIndexRange> ranges, in CellModes modes)
+        {
+            if (ranges.Count > 0 && pivot.HasValue)
+            {
+                switch (modes.Diagonal)
+                {
+                    case CellMode.Exclude:
+                        RemoveDiagonal(pivot.Value, ranges);
+                        break;
+
+                    case CellMode.Only:
+                        OnlyDiagonal(pivot.Value, ranges);
+                        break;
+                }
+            }
+
+            if (ranges.Count > 0 && modes.Occupied != CellMode.Include)
             {
                 var occupied = ListPool<GridIndex>.Get();
                 this.occupier.GetOccupied(occupied);
                 occupied.Sort(new GridIndex1Comparer(this.data.Size, true));
 
-                switch (cModes.Occupied)
+                switch (modes.Occupied)
                 {
                     case CellMode.Exclude:
                         RemoveOccupied(ranges, occupied);
@@ -337,19 +448,34 @@ namespace HegaCore
 
                 ListPool<GridIndex>.Return(occupied);
             }
+        }
 
-            if (ranges.Count <= 0)
+        private void RemoveDiagonal(in GridIndexRange pivot, List<GridIndexRange> ranges)
+        {
+            var normalPivot = pivot.Normalize();
+
+            for (var i = ranges.Count - 1; i >= 0; i--)
             {
-                ListPool<GridIndexRange>.Return(ranges);
-                return false;
+                var normal = ranges[i].Normalize();
+
+                if (normal.Start.Row != normalPivot.Start.Row && normal.Start.Column != normalPivot.Start.Column &&
+                    normal.End.Row != normalPivot.End.Row && normal.End.Column != normalPivot.End.Column)
+                    ranges.RemoveAt(i);
             }
+        }
 
-            var random = ListPool<GridIndexRange>.Get();
-            random.AddRange(ranges.Randomize());
-            range = random[UnityRandom.Range(0, random.Count)];
+        private void OnlyDiagonal(in GridIndexRange pivot, List<GridIndexRange> ranges)
+        {
+            var normalPivot = pivot.Normalize();
 
-            ListPool<GridIndexRange>.Return(random, ranges);
-            return true;
+            for (var i = ranges.Count - 1; i >= 0; i--)
+            {
+                var normal = ranges[i].Normalize();
+
+                if (normal.Start.Row == normalPivot.Start.Row || normal.Start.Column == normalPivot.Start.Column ||
+                    normal.End.Row == normalPivot.End.Row || normal.End.Column == normalPivot.End.Column)
+                    ranges.RemoveAt(i);
+            }
         }
 
         private void RemoveOccupied(List<GridIndexRange> ranges, List<GridIndex> occupied)
