3dcd00b5c76e827385b58f52a5c13a487eed1939
Laicasaane <laicasaane@outlook.com>
2020-10-06 19:48:00 +0700

    Update

---
+++ Scripts/UI/GraphicOnClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace HegaCore.UI
+{
+    public class GraphicOnClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField]
+        private Graphic graphic = null;
+
+        [SerializeField]
+        private Color color = Color.white;
+
+        private Color backupColor;
+
+        private void Awake()
+        {
+            this.backupColor = this.graphic.color;
+        }
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            this.graphic.color = this.backupColor;
+        }
+
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            this.graphic.color = this.color;
+        }
+    }
+}
