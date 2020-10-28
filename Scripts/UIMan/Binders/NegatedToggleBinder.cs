dcb5e7cfb168958a2e3b66e6dcc958754b7ca97b
Laicasaane <laicasaane@outlook.com>
2020-10-28 21:45:34 +0700

    Update

---
+++ Scripts/UIMan/Binders/NegatedToggleBinder.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnuGames.MVVM;
+
+namespace HegaCore.MVVM
+{
+    [RequireComponent(typeof(Toggle))]
+    [DisallowMultipleComponent]
+    public class NegatedToggleBinder : BinderBase
+    {
+        public bool ignoreGroup = false;
+
+        protected Toggle toggle;
+
+        [HideInInspector]
+        public BindingField valueField = new BindingField("Value");
+
+        [HideInInspector]
+        public TwoWayBindingBool onValueChanged = new TwoWayBindingBool("On Value Changed");
+
+        [HideInInspector]
+        public BoolConverter valueConverter = new BoolConverter("Value");
+
+        public override void Initialize(bool forceInit)
+        {
+            if (!CheckInitialize(forceInit))
+                return;
+
+            this.toggle = GetComponent<Toggle>();
+
+            SubscribeOnChangedEvent(this.valueField, OnUpdateValue);
+
+            OnValueChanged_OnChanged(this.onValueChanged);
+            this.onValueChanged.onChanged += OnValueChanged_OnChanged;
+        }
+
+        private void OnUpdateValue(object val)
+        {
+            var value = this.valueConverter.Convert(val, this);
+            this.toggle.SetIsOnWithoutNotify(!value);
+        }
+
+        private void OnValueChanged(bool value)
+        {
+            value = !value;
+
+            if (!this.ignoreGroup && !value && this.toggle.group)
+                return;
+
+            SetValue(this.valueField, this.onValueChanged.converter.Convert(value, this));
+        }
+
+        private void OnValueChanged_OnChanged(bool value)
+        {
+            this.toggle.onValueChanged.RemoveListener(OnValueChanged);
+
+            if (value)
+                this.toggle.onValueChanged.AddListener(OnValueChanged);
+        }
+    }
+}
---
+++ Scripts/UIMan/Binders/PanelsBinder.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnuGames.MVVM;
+
+namespace HegaCore.MVVM
+{
+    using UI;
+
+    public class PanelsBinder : BinderBase
+    {
+        public List<Panel> showOnTrue = new List<Panel>();
+        public List<Panel> hideOnTrue = new List<Panel>();
+
+        [HideInInspector]
+        public BindingField valueField = new BindingField("bool");
+
+        [HideInInspector]
+        public BoolConverter valueConverter = new BoolConverter("bool");
+
+        public override void Initialize(bool forceInit)
+        {
+            if (!CheckInitialize(forceInit))
+                return;
+
+            SubscribeOnChangedEvent(this.valueField, OnUpdateValue);
+        }
+
+        private void OnUpdateValue(object val)
+        {
+            var valChange = this.valueConverter.Convert(val, this);
+
+            if (this.showOnTrue != null && this.showOnTrue.Count > 0)
+            {
+                for (var i = 0; i < this.showOnTrue.Count; i++)
+                {
+                    if (!this.showOnTrue[i])
+                        continue;
+
+                    if (valChange)
+                        this.showOnTrue[i].Show();
+                    else
+                        this.showOnTrue[i].Hide();
+                }
+            }
+
+            if (this.hideOnTrue != null && this.hideOnTrue.Count > 0)
+            {
+                for (var i = 0; i < this.hideOnTrue.Count; i++)
+                {
+                    if (!this.hideOnTrue[i])
+                        continue;
+
+                    if (valChange)
+                        this.hideOnTrue[i].Hide();
+                    else
+                        this.hideOnTrue[i].Show();
+                }
+            }
+        }
+    }
+}
