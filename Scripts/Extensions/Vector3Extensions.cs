4ec911c6f70b11055dfa2fb10bbbe3cf4ccfcf3d
Laicasaane <laicasaane@outlook.com>
2020-11-05 21:37:22 +0700

    Update

---
+++ Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HegaCore
+{
+    public static class Vector3Extensions
+    {
+        public static Vector3 ZSortingOrder(in this Vector3 self, int order)
+        {
+            var z = Mathf.Abs(self.y) * order;
+            return self.With(z: z);
+        }
+    }
+}
