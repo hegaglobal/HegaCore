8ecebc065468310cf528fa01e3e866a3f51efefe
Laicasaane <laicasaane@outlook.com>
2020-11-07 18:31:07 +0700

    Update

--- Scripts/Common/GameMode.cs
+++ Scripts/Common/GameMode.cs
@@ -6,4 +6,24 @@
         Easy,
         Hard
     }
+
+    public static class GameModeExtensions
+    {
+        public static class SimpleName
+        {
+            public const string Normal = "N";
+            public const string Easy = "E";
+            public const string Hard = "H";
+        }
+
+        public static string ToSimpleName(this GameMode self)
+        {
+            switch (self)
+            {
+                case GameMode.Easy: return SimpleName.Easy;
+                case GameMode.Hard: return SimpleName.Hard;
+                default: return SimpleName.Normal;
+            }
+        }
+    }
 }
--- Scripts/UIMan/Modules/VersionModule.cs
+++ Scripts/UIMan/Modules/VersionModule.cs
@@ -3,11 +3,14 @@ using UnuGames;
 
 namespace HegaCore.UI
 {
-    public sealed class VersionModule : UIManModule<VersionViewModel>
+    public sealed class VersionModule : SingletonModule<VersionModule, VersionViewModel>
     {
         private void Start()
         {
             this.DataInstance.Version = Application.version;
         }
+
+        public void SetGameMode(GameMode mode)
+            => this.DataInstance.Version = $"{Application.version} ({mode.ToSimpleName()})";
     }
 }
