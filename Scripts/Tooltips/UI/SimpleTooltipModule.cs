52964059ed61cdbe6b736b98ced969fec48d8251
Laicasaane <laicasaane@outlook.com>
2020-11-08 15:20:44 +0700

    Update

--- Scripts/Tooltips/UI/SimpleTooltipModule.cs
+++ Scripts/Tooltips/UI/SimpleTooltipModule.cs
@@ -1,11 +1,20 @@
-﻿namespace HegaCore.UI
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace HegaCore.UI
 {
     public sealed class SimpleTooltipModule : TooltipModule
     {
+        [SerializeField, BoxGroup("L10n Key Format")]
+        private string contentFormat = string.Empty;
+
         public override void Set(string l10nKey, IToTemplatedString template = null)
         {
             if (TrySetDefault(template))
                 return;
+
+            if (!string.IsNullOrEmpty(this.contentFormat))
+                l10nKey = string.Format(this.contentFormat, l10nKey);
 
             var content = (template ?? TooltipData.None).ToTemplatedString(L10n.Localize(l10nKey));
             SetContent(content);
--- Scripts/Tooltips/UI/TitledTooltipModule.cs
+++ Scripts/Tooltips/UI/TitledTooltipModule.cs
@@ -1,7 +1,16 @@
-﻿namespace HegaCore.UI
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace HegaCore.UI
 {
     public sealed class TitledTooltipModule : TooltipModule
     {
+        [SerializeField, BoxGroup("L10n Key Format")]
+        private string titleFormat = string.Empty;
+
+        [SerializeField, BoxGroup("L10n Key Format")]
+        private string contentFormat = string.Empty;
+
         public override void Set(string l10nKey, IToTemplatedString template = null)
         {
             if (TrySetDefault(template))
@@ -9,10 +18,19 @@
 
             var tempData = template ?? TooltipData.None;
 
-            var title = TooltipData.None.ToTemplatedString(L10n.Localize($"{l10nKey}-title"));
+            var titleKey = l10nKey;
+            var contentKey = l10nKey;
+
+            if (!string.IsNullOrEmpty(this.titleFormat))
+                titleKey = string.Format(this.titleFormat, l10nKey);
+
+            if (!string.IsNullOrEmpty(this.contentFormat))
+                contentKey = string.Format(this.contentFormat, l10nKey);
+
+            var title = TooltipData.None.ToTemplatedString(L10n.Localize(titleKey));
             SetTitle(title);
 
-            var content = tempData.ToTemplatedString(L10n.Localize(l10nKey));
+            var content = tempData.ToTemplatedString(L10n.Localize(contentKey));
             SetContent(content);
         }
     }
