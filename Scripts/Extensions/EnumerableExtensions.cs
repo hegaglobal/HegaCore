950edc1b1be0d0ca6188eec50367a343cd18ded1
Laicasaane <laicasaane@outlook.com>
2021-01-20 21:30:02 +0700

    Update

---
+++ Scripts/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections.Pooling;
+
+namespace HegaCore
+{
+    public static class EnumerableExtensions
+    {
+        public static void Exclude<T>(this IEnumerable<T> source, ICollection<T> dest, params T[] excludes)
+        {
+            var set = Pool.Provider.HashSet<T>();
+            set.AddRange(excludes);
+
+            foreach (var type in source)
+            {
+                if (set.Contains(type))
+                    continue;
+
+                dest.Add(type);
+            }
+
+            Pool.Provider.Return(set);
+        }
+
+        public static IEnumerable<T> Exclude<T>(this IEnumerable<T> source, params T[] excludes)
+        {
+            var set = Pool.Provider.HashSet<T>();
+            set.AddRange(excludes);
+
+            foreach (var type in source)
+            {
+                if (set.Contains(type))
+                    continue;
+
+                yield return type;
+            }
+
+            Pool.Provider.Return(set);
+        }
+    }
+}
