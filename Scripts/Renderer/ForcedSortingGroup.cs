e5c074f1000fe9a519b9c434b6ae616642e1f8f0
Laicasaane <laicasaane@outlook.com>
2021-04-07 10:50:08 +0700

    Update

---
+++ Scripts/Renderer/ForcedSortingGroup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace HegaCore
+{
+    public class ForcedSortingGroup : MonoBehaviour
+    {
+        [SerializeField]
+        private SortingLayerId sortingLayer;
+
+        [SerializeField, Min(0)]
+        private int orderInLayer = 0;
+
+        private Renderer[] renderers = new Renderer[0];
+
+        private void Awake()
+        {
+            this.renderers = GetComponentsInChildren<Renderer>();
+
+            foreach (var renderer in this.renderers)
+            {
+                renderer.sortingLayerID = this.sortingLayer.id;
+                renderer.sortingOrder = this.orderInLayer;
+            }
+        }
+    }
+}
