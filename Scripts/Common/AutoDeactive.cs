5f26b70a4785814289890dc18e81c76017252d92
Dong PC <dong.vtcgd@gmail.com>
2022-12-17 17:44:30 +0700

    add

---
+++ Scripts/Common/AutoDeactive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Live2D.Cubism.Core.Unmanaged;
+using UnityEngine;
+
+public class AutoDeactive : MonoBehaviour
+{
+    public float duration = 1f;
+
+    void OnEnable()
+    {
+        StartCoroutine(DeactiveCO(duration));
+    }
+
+    IEnumerator DeactiveCO(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+}
