7c87f87103b7b5515520d9916867fedfd6a0cb65
Dong PC <dolphung0708@gmail.com>
2024-02-25 17:11:57 +0700

    update steam manager (remove always check A1 ID)

--- Steamworks/SteamManager.cs
+++ Steamworks/SteamManager.cs
@@ -51,8 +51,6 @@ public class SteamManager : MonoBehaviour {
 	}
 
 	protected virtual void Awake() {
-
-		
 		// Only one instance of SteamManager at a time!
 		if (s_instance != null) {
 			Destroy(gameObject);
@@ -69,7 +67,7 @@ public class SteamManager : MonoBehaviour {
 			gameObject.SetActive(false);
 			return;
 		}
-
+		
 		Debug.Log("SteamMamager INIT SUCCESSFULLY !!!!!!!!!!!!!!!");
 		
 		if (s_EverInitialized) {
@@ -94,19 +92,28 @@ public class SteamManager : MonoBehaviour {
 		try {
 			// If Steam is not running or the game wasn't started through Steam, SteamAPI_RestartAppIfNecessary starts the
 			// Steam client and also launches this game again if the User owns it. This can act as a rudimentary form of DRM.
-
+		
 			// Once you get a Steam AppID assigned by Valve, you need to replace AppId_t.Invalid with it and
 			// remove steam_appid.txt from the game depot. eg: "(AppId_t)480" or "new AppId_t(480)".
 			// See the Valve documentation for more information: https://partner.steamgames.com/doc/sdk/api#initialization_and_shutdown
-			if (SteamAPI.RestartAppIfNecessary((AppId_t)1812060)) {
-				Application.Quit();
+			if (SteamAPI.RestartAppIfNecessary(AppId_t.Invalid)) 
+			{
+#if DEMO
+				gameObject.SetActive(false);		
+#else
+				Application.Quit(); 
+#endif
 				return;
 			}
 		}
-		catch (System.DllNotFoundException e) { // We catch this exception here, as it will be the first occurrence of it.
+		catch (System.DllNotFoundException e) 
+		{ // We catch this exception here, as it will be the first occurrence of it.
+#if DEMO
+			gameObject.SetActive(false);		
+#else
 			Debug.LogError("[Steamworks.NET] Could not load [lib]steam_api.dll/so/dylib. It's likely not in the correct location. Refer to the README for more details.\n" + e, this);
-
 			Application.Quit();
+#endif
 			return;
 		}
 
